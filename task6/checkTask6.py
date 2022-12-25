dea8b360ddfcc3f2b68820dbd473297c61d538bc
komtriangle <komlevdanila742@gmail.com>
2022-12-25 15:21:37 +0300

    task6

---
+++ task6/checkTask6.py
@@ -0,0 +1,46 @@
+import json
+import task6
+
+
+def result_to_list(res):
+    try:
+        return json.loads(res)
+    except Exception:
+        return res
+
+
+def data_validation(result_data, data_for_validation):
+    diff = []
+    r = []
+    for i in range(3):
+        d = abs(result_data[i]-data_for_validation[i])
+        diff.append(d)
+        r.append(d < e)
+    return [r, diff]
+
+
+def validate(data_validation_result):
+    if max(data_validation_result[1]) < e:
+        return 'OK'
+    else:
+        return data_validation_result[1]
+
+
+def exec_validation(input_data, validation_set):
+    try:
+        result = task6.task(input_data)
+        return validate(data_validation(result_to_list(result), validation_set))
+    except Exception:
+        None
+
+
+data_json = "[[1,3,2],[2,2,2],[1.5,3,1.5]]"
+data_csv = "1,3,2 \n2,2,2\n1.5,3,1.5"
+validation_data = json.loads("[0.468, 0.169, 0.363]")
+e = 0.01
+
+
+for data_string in [data_json, data_csv]:
+    r = exec_validation(data_string, validation_data)
+    if r:
+        print(r)
---
+++ task6/task6.py
@@ -0,0 +1,46 @@
+import json
+import numpy as np
+
+
+def task(json_string):
+    data = json.loads(json_string)
+    matrixes = []
+    for i in range(len(data)):
+        matrixes.append([])
+        for j in range(len(data[0])):
+            matrixes[i].append([0]*len(data[0]))
+    for idx, expert in enumerate(data):
+        for i in range(len(expert)):
+            for j in range(len(expert)):
+                value = 0.5
+                if expert[i] > expert[j]:
+                    value = 1
+                elif expert[i] < expert[j]:
+                    value = 0
+                matrixes[idx][i][j] = value
+    mean_matrix = []
+    for i in range(len(data[0])):
+        mean_matrix.append([0]*len(data[0]))
+        for j in range(len(data[0])):
+            temp = 0.0
+            temp = sum([matrixes[k][i][j] for k in range(len(matrixes))])
+            temp /= len(matrixes)
+            mean_matrix[i][j] = temp
+
+    m_matrix = np.array(mean_matrix)
+    m_matrix = np.transpose(m_matrix)
+
+    k_0 = [1/len(data) for _ in range(len(data))]
+    k = k_0
+    one_arr = np.array([1, 1, 1])
+    e = 0.001
+
+    is_first = True
+    while is_first or max(abs(k-k_0)) >= e:
+        is_first = False
+        k_0 = k
+        y = np.dot(m_matrix, k_0)
+        l = np.dot(one_arr, y)
+        k = np.dot(1/l, y)
+
+    return k
